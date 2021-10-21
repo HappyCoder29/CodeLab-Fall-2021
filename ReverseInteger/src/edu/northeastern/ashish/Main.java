7bcaa3e7ad43eb0f4182c8c2f3fcc15055650ba4
Ashish <ashish@drillmaps.com>
2021-10-21 11:11:29 -0700

    Making changes for figuring out overflow.

--- ReverseInteger/src/edu/northeastern/ashish/Main.java
+++ ReverseInteger/src/edu/northeastern/ashish/Main.java
@@ -6,6 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
         System.out.println(reverseInt(-123));
     }
 
@@ -13,22 +16,26 @@ public class Main {
         if(num == 0 ){
             return num;
         }
-        boolean isnegative = num < 0;
 
-        if(isnegative){
-            num = num * -1;
-        }
 
         int answer = 0;
 
         while(num != 0){
+
+            int pop = num % 10;
+
+            if (answer > Integer.MAX_VALUE/10 || (answer == Integer.MAX_VALUE / 10 && pop > 7)){
+                return 0;
+            }
+            if (answer < Integer.MIN_VALUE/10 || (answer == Integer.MIN_VALUE / 10 && pop < -8)){
+                return 0;
+            }
+
             answer = answer* 10 + num % 10;
             num = num / 10;
         }
 
-        if(isnegative){
-            answer = answer * -1;
-        }
+
 
         return answer;
     }
